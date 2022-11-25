fe94d403c653f163971cb3c260a41dc01b4a2d82
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-25 17:17:51 +0900

    add navigation after making new quote with useHistory

--- src/components/pages/NewQuote.tsx
+++ src/components/pages/NewQuote.tsx
@@ -1,8 +1,13 @@
 import QuoteForm from "@/components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 function NewQuote() {
+  const history = useHistory();
+
   const addQuoteHandler = (quote: {author: string | undefined, text: string | undefined}) => {
     console.log(quote);
+
+    history.push('/quotes');
   }
 
   return (
