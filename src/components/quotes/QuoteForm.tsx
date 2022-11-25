ef5f7a1bbd6ff053938bebed2b4ab0cc195ce5d9
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-25 17:27:29 +0900

    add prompt notification when go back page while inputting in QuoteForm

--- src/components/quotes/QuoteForm.tsx
+++ src/components/quotes/QuoteForm.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { CardDiv } from '@/components/UI/CardStyle';
 import {
   QuoteFormActionButton,
@@ -8,15 +8,20 @@ import {
   QuoteFormLoadingDiv,
 } from '@/components/quotes/QuoteFormStyle';
 import { LoadingSpinnerDiv } from '../UI/LoadingSpinnerStyle';
+import { Prompt } from 'react-router-dom';
 
 interface QuoteFormProps {
   isLoading: boolean;
-  onAddQuote: (quote: {author: string | undefined, text: string | undefined}) => void;
+  onAddQuote: (quote: {
+    author: string | undefined;
+    text: string | undefined;
+  }) => void;
 }
 
 function QuoteForm({ isLoading, onAddQuote }: QuoteFormProps) {
   const authorInputRef = useRef<HTMLInputElement>(null);
   const textInputRef = useRef<HTMLTextAreaElement>(null);
+  const [isInputting, setIsInputting] = useState(false);
 
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -24,35 +29,54 @@ function QuoteForm({ isLoading, onAddQuote }: QuoteFormProps) {
     const authorInput = authorInputRef.current?.value;
     const textInput = textInputRef.current?.value;
 
-    onAddQuote({author: authorInputRef.current?.value, text: textInputRef.current?.value});
+    onAddQuote({
+      author: authorInputRef.current?.value,
+      text: textInputRef.current?.value,
+    });
   };
 
+  const formFocusHandler = () => {
+    setIsInputting(true);
+  };
+
+  const finishInputHandler = () => {
+    setIsInputting(false);
+  }
+
   return (
-    <CardDiv>
-      <QuoteFormForm onSubmit={submitHandler}>
-        {isLoading && (
-          <QuoteFormLoadingDiv>
-            <LoadingSpinnerDiv className='spinner' />
-          </QuoteFormLoadingDiv>
-        )}
-        <QuoteFormControlDiv>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </QuoteFormControlDiv>
-        <QuoteFormControlDiv>
-          <label htmlFor="text">Text</label>
-          <textarea
-            name="text"
-            id="text"
-            rows={5}
-            ref={textInputRef}
-          ></textarea>
-        </QuoteFormControlDiv>
-        <QuoteFormActionsDiv>
-          <QuoteFormActionButton>Add Quote</QuoteFormActionButton>
-        </QuoteFormActionsDiv>
-      </QuoteFormForm>
-    </CardDiv>
+    <>
+      <Prompt
+        when={isInputting}
+        message={(location) =>
+          'Are you sure to leave? All input data will be lost'
+        }
+      />
+      <CardDiv>
+        <QuoteFormForm onSubmit={submitHandler} onFocus={formFocusHandler}>
+          {isLoading && (
+            <QuoteFormLoadingDiv>
+              <LoadingSpinnerDiv className="spinner" />
+            </QuoteFormLoadingDiv>
+          )}
+          <QuoteFormControlDiv>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </QuoteFormControlDiv>
+          <QuoteFormControlDiv>
+            <label htmlFor="text">Text</label>
+            <textarea
+              name="text"
+              id="text"
+              rows={5}
+              ref={textInputRef}
+            ></textarea>
+          </QuoteFormControlDiv>
+          <QuoteFormActionsDiv>
+            <QuoteFormActionButton onClick={finishInputHandler}>Add Quote</QuoteFormActionButton>
+          </QuoteFormActionsDiv>
+        </QuoteFormForm>
+      </CardDiv>
+    </>
   );
 }
 
